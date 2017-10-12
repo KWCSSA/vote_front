48ef22477610721ffe1f49b4d70bf3a1e028a831
Tony Xie <651376379@qq.com>
2017-10-12 19:46:58 -0400

    updated logger

--- backend/logger.js
+++ backend/logger.js
@@ -1,13 +1,18 @@
-var util = require( 'util' );
-var colors = require( 'colors' );
+var winston = require('winston');
+var fs = require('fs');
 
+const logDir = 'log';
 
-module.exports.errLog = function( msg ) {
-	util.log( msg );
-};
+if (!fs.existsSync(logDir)){
+	fs.mkdirSync(logDir);
+}
 
+var logger = new(winston.Logger)({
+	level: 'info',
+	transports: [
+		new winston.transports.File({ name: 'error', filename: `./${logDir}/error.log`, level: 'error' }),
+    	new winston.transports.File({ name: 'other', filename: `./${logDir}/combined.log` })
+	]
+});
 
-module.exports.infoLog = function( msg ) {
-	util.log( msg.cyan );
-};
-
+module.exports = logger;
